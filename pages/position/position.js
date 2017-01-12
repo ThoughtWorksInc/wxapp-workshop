97e2520aa9e3f39a9e9dc4f14284b315571e93b5
HelloRuiLi <1229668446@qq.com>
2017-01-12 18:24:22 +0800

    simplify form

--- pages/position/position.js
+++ pages/position/position.js
@@ -1,72 +1,33 @@
+const fields = [
+  {
+    name: 'jobName',
+    tag: 'input',
+    placeholder: '职位名称',
+  },
+  {
+    name: 'address',
+    tag: 'input',
+    placeholder: '您所在的地点',
+  },
+  {
+    name: 'expectAddress',
+    tag: 'input',
+    placeholder: '您期望的办公地点',
+  },
+  {
+    name: 'description',
+    tag: 'textarea',
+    placeholder: '职位描述',
+  },
+];
+
 Page({
   data: {
     values: {},
-    fields: [
-      {
-        name: 'jobName',
-        tag: 'input',
-        placeholder: '职位名称',
-      },
-      {
-        name: 'description',
-        tag: 'textarea',
-        placeholder: '职位描述',
-      },
-      {
-        name: 'address',
-        tag: 'select',
-        placeholder: '您所在的地点',
-        options: [
-          {
-            label: '成都',
-            value: 'chengdu',
-          },
-          {
-            label: '北京',
-            value: 'beijing',
-          },
-        ]
-      },
-      {
-        name: 'expectAddress',
-        tag: 'select',
-        placeholder: '您期望的工作地点',
-        options: [
-          {
-            label: '成都',
-            value: 'chengdu',
-          },
-          {
-            label: '北京',
-            value: 'beijing',
-          },
-        ]
-      },
-    ]
+    fields
   },
-
-  onChange(e){
-    const fieldValues = Object.assign(this.data.values, {
-      [e.target.dataset.name]: e.detail.value,
-    });
-    this.setData({
-      values: fieldValues,
-    });
-  },
-
   onSubmit(e){
-    const pickers = this.data.fields.filter((field) => {
-      return field.tag === 'select';
-    });
-
     const values = e.detail.value;
-    pickers.forEach((picker) => {
-      const index = this.data.values[picker.name];
-      Object.assign(values, {
-        [picker.name]: (picker.options[index] || {}).value,
-      });
-    });
-
     console.log(values);
   }
 });
