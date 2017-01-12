dfb3ba8a4db19498ac76a68ef549ccd4a06cd4b5
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-01-12 15:32:40 +0800

    [#0] - Added other initial pages

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -1,5 +1,7 @@
 //logs.js
-var util = require('../../utils/util.js')
+// var util = require('../../utils/util.js')
+import { formatTime } from '../../utils/util.js'
+
 Page({
   data: {
     logs: []
@@ -7,7 +9,7 @@ Page({
   onLoad: function () {
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
+        return formatTime(new Date(log))
       })
     })
   }
--- pages/position/position.js
+++ pages/position/position.js
@@ -1,4 +1,4 @@
-//index.js
+//position.js
 //获取应用实例
 var app = getApp()
 Page({
@@ -7,19 +7,19 @@ Page({
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function () {
+  bindViewTap: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../explore/explore'
     })
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
+    app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
-        userInfo: userInfo
+        userInfo:userInfo
       })
     })
   }
